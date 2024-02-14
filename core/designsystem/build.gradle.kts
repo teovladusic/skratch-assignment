2938cde442e06fa695f67c99257909420ce21668
Teo Vladušić <teo.vladusic@gmail.com>
2024-02-14 15:00:51 +0100

    Design system setup (#1)
    Design system setup: Create a separate module/submodule :core:designsystem, Colors and Thems, Typography and fonts

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -1,24 +1,18 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.androidLibrary)
     alias(libs.plugins.kotlinAndroid)
 }
 
 android {
-    namespace = "com.teovladusic.skratchassignment"
+    namespace = "com.teovladusic.core.designsystem"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.teovladusic.skratchassignment"
         minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -30,6 +24,12 @@ android {
             )
         }
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.androidxComposeCompiler.get().toString()
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -37,24 +37,12 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.androidxComposeCompiler.get().toString()
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
 }
 
 dependencies {
-    implementation(project(":core:designsystem"))
 
     implementation(libs.core.ktx)
-    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.appcompat)
     implementation(libs.activity.compose)
     implementation(platform(libs.compose.bom))
     implementation(libs.ui)
@@ -64,8 +52,4 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    androidTestImplementation(platform(libs.compose.bom))
-    androidTestImplementation(libs.ui.test.junit4)
-    debugImplementation(libs.ui.tooling)
-    debugImplementation(libs.ui.test.manifest)
 }
---
+++ core/designsystem/src/main/java/com/teovladusic/core/designsystem/theme/Color.kt
@@ -0,0 +1,28 @@
+package com.teovladusic.core.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(color = 0xFFD0BCFF)
+val PurpleGrey80 = Color(color = 0xFFCCC2DC)
+val Pink80 = Color(color = 0xFFEFB8C8)
+
+val Purple40 = Color(color = 0xFF6650a4)
+val PurpleGrey40 = Color(color = 0xFF625b71)
+val Pink40 = Color(color = 0xFF7D5260)
+
+val SkratchNavy = Color(color = 0xFF2A2E43)
+val SkratchNavy50 = Color(color = 0xFF9496A1)
+
+val SkratchWhite = Color(color = 0xFFFFFFFF)
+
+val NewPrimary = Color(color = 0xFF5626F4)
+
+val SkratchPaleBlue = Color(color = 0xFFD2D4E3)
+val SkratchPaleBlue50 = Color(color = 0xFFE9E9F1)
+
+val SkratchBlack = Color(color = 0xFF000000)
+
+val SkratchText = Color(color = 0xFFBDBDBD)
+val SkratchTextDark = Color(color = 0xFFB2B2B2)
+
+val SkratchUltralight = Color(color = 0xFFF6F6F6)
