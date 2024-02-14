84b1c9549f19be78278728751ee6ac6782562c66
Teo Vladušić <teo.vladusic@gmail.com>
2024-02-14 14:06:14 +0100

    Colors and Theme setup

--- core/designsystem/src/main/java/com/teovladusic/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/teovladusic/core/designsystem/theme/Theme.kt
@@ -16,15 +16,15 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = NewPrimary,
+    secondary = SkratchWhite,
+    tertiary = SkratchWhite
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = NewPrimary,
+    secondary = SkratchWhite,
+    tertiary = SkratchWhite
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
