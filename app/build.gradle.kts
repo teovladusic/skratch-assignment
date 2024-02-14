a214ca6e88eb4c849bbdd2ae6613054259c313ac
Teo Vladušić <teo.vladusic@gmail.com>
2024-02-14 15:20:47 +0100

    setup hilt for dependency injection (#2)
    setup hilt for dependency injection

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -61,11 +63,18 @@ dependencies {
     implementation(libs.ui.graphics)
     implementation(libs.ui.tooling.preview)
     implementation(libs.material3)
+
+    // Dagger Hilt
+    ksp(libs.dagger.hilt.compiler)
+    implementation(libs.bundles.dagger.hilt)
+
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
     androidTestImplementation(platform(libs.compose.bom))
     androidTestImplementation(libs.ui.test.junit4)
+
     debugImplementation(libs.ui.tooling)
     debugImplementation(libs.ui.test.manifest)
 }
---
+++ app/src/main/java/com/teovladusic/skratchassignment/SkratchAssignmentApplication.kt
@@ -0,0 +1,7 @@
+package com.teovladusic.skratchassignment
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SkratchAssignmentApplication: Application()
